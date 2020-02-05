32c9ec7aefea4614ec05a4dc2f5d4ca4696babb8
jawilliams24 <jwilliams9517@gmail.com>
2020-02-05 13:25:41 +0000

    first commit

---
+++ src/com/qa/fact/Factorials.java
@@ -0,0 +1,28 @@
+package com.qa.fact;
+
+import java.util.Scanner;
+
+public class Factorials {
+
+	static int number;
+	
+	public static Integer number() {
+		Scanner numberScanner = new Scanner(System.in);
+		number = numberScanner.nextInt();
+		
+		return number;
+		
+		
+	
+}
+	
+	public calc() {
+		
+		for(int i=0;i<99999999;i++) {
+			
+			
+		}
+	}
+	
+	
+}
---
+++ src/com/qa/fact/Runner.java
@@ -0,0 +1,13 @@
+package com.qa.fact;
+
+import java.util.Scanner;
+
+public class Runner {
+	public static void main(String[] args) {
+
+		System.out.println("Enter your number here: ");
+		Integer number = Factorials.number();
+		System.out.println(number);
+		
+	}
+}
